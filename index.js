4db88a53278e7ddcae2f48d2cdb60208ea72117e
Leonardo Motta <64990810+leo-motta@users.noreply.github.com>
2022-12-20 13:22:32 +0000

    update logs  API endpoint

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ let exerciseSchema = new mongoose.Schema({
     required: true
   },
   date: {
-    type: String,
+    type: Date,
     required: true
   },
   duration: {
@@ -72,19 +72,22 @@ const createAndSaveUser = async (username) => {
 //Find one user by ID
 const findUserById = (userId) => {
   return new Promise((resolve, reject) => {
-    User.findById(userId).lean().exec((err, user) => {
-      if(err) reject(err)
-      if(user) resolve(user)
-    })
+    User.findById(userId)
+      .select("-__v")
+      .lean()
+      .exec((err, user) => {
+        if (err) reject(err)
+        if (user) resolve(user)
+      })
   })
 }
 
 //Get list of Users
 const getUsers = async () => {
   return new Promise((resolve, reject) => {
-    User.find({},(err, data) => {
-      if(err) reject(err)
-      if(data) resolve(data)
+    User.find({}, (err, doc) => {
+      if (err) reject(err)
+      if (doc) resolve(doc)
     })
   })
 }
@@ -95,8 +98,8 @@ const createAndSaveExercise = async (user_id, date, duration, description) => {
     const exercise = new Exercise({
       user_id: user_id,
       date: date,
-      duration:duration,
-      description:description
+      duration: duration,
+      description: description
     })
     exercise.save((err, doc) => {
       if (err) reject(err)
@@ -106,12 +109,19 @@ const createAndSaveExercise = async (user_id, date, duration, description) => {
 }
 
 //Find Exercises
-const findExercisesByUserId = async (user_id) => {
+const findExercisesByUserId = async (user_id, date_from, date_to, limit) => {
   return new Promise((resolve, reject) => {
-    Exercise.find({user_id:user_id}).lean().exec((err, data) => {
-      if(err) reject(err)
-      if(data) resolve(data)
+    Exercise.find({
+      user_id: user_id,
+      date: { $gte: date_from, $lte: date_to }
     })
+      .select("-_id -user_id -__v")
+      .limit(limit)
+      .lean()
+      .exec((err, doc) => {
+        if (err) reject(err)
+        if (doc) resolve(doc)
+      })
   })
 }
 
@@ -121,7 +131,7 @@ app.route('/api/users').post(async (req, res) => {
     const doc = await createAndSaveUser(req.body.username)
     //The returned response from POST /api/users with form data username will be an object with username and _id properties
     if (doc)
-      res.json({"username": doc.username, "_id": doc._id})
+      res.json({ "username": doc.username, "_id": doc._id })
   } catch (err) {
     console.log(err)
   }
@@ -130,79 +140,70 @@ app.route('/api/users').post(async (req, res) => {
 //You can make a GET request to /api/users to get a list of all users.
 //Each element in the array returned from GET /api/users is an object literal containing a user's username and _id.
 app.route('/api/users').get(async (req, res) => {
-  try {    
+  try {
     const users = await getUsers()
-    if(users)
+    if (users)
       res.json(users) //The GET request to /api/users returns an array.
-  } catch(err) {
+  } catch (err) {
     console.log(err)
   }
 })
 
 //You can POST to /api/users/:_id/exercises with form data description, duration, and optionally date. If no date is supplied, the current date will be used.
 app.route('/api/users/:_id/exercises').post(async (req, res) => {
-  try {        
+  try {
     //Find user
     let user = await findUserById(req.params._id)
-    if(!user)
+    if (!user)
       throw new Error('user not found!')
 
     const user_id = req.params._id
     const duration = req.body.duration
     const description = req.body.description
-    
-    //Date
-    let date
-    if(req.body.date)
-      date =  new Date(req.body.date)
-    else
-      date = new Date()
-    const date_string = date.toDateString()
+    const date = (req.body.date) ? new Date(req.body.date) : new Date()
 
     //Create exercise
-    const exercise = await createAndSaveExercise(user_id, date_string, duration, description)
-    if(!exercise)
+    const exercise = await createAndSaveExercise(user_id, date, duration, description)
+    if (!exercise)
       throw new Error('exercise not created!')
 
     //The response returned from POST /api/users/:_id/exercises will be the user object with the exercise fields added.
-    user.date = exercise.date
+    user.date = exercise.date.toDateString()
     user.duration = exercise.duration
     user.description = exercise.description
-    delete user.__v
     res.json(user)
-    
-  } catch(err) {
+
+  } catch (err) {
     console.log(err)
   }
 })
 
-//https://exercisetracker.leonardomotta.repl.co/api/users/63a0dd3fb8837f023889d3ad/logs
-
 //You can make a GET request to /api/users/:_id/logs to retrieve a full exercise log of any user.
 //You can add from, to and limit parameters to a GET /api/users/:_id/logs request to retrieve part of the log of any user. from and to are dates in yyyy-mm-dd format. limit is an integer of how many logs to send back.
 app.route('/api/users/:_id/logs').get(async (req, res) => {
-  try {    
+  try {
+    const date_from = req.query.from || new Date(0)
+    const date_to = req.query.to || new Date(Date.now())
+    const limit = Number(req.query.limit) || 10
+
     const user = await findUserById(req.params._id)
-    if(!user)
+    if (!user)
       throw new Error('user not found!')
 
-    const exercises = await findExercisesByUserId(user._id)
+    const exercises = await findExercisesByUserId(user._id, date_from, date_to, limit)
 
-    if(!exercises)
+    if (!exercises)
       throw new Error('no exercises found!')
-    
-    exercises.forEach((ex) => { 
-      delete ex._id
-      delete ex.user_id
-      delete ex.__v
+
+    exercises.forEach((ex) => {
+      ex.date = ex.date.toDateString()
     })
-    
+
     user.count = exercises.length
     user.log = exercises
-    delete user.__v
     res.json(user)
-    
-  } catch(err) {
+
+  } catch (err) {
     console.log(err)
   }
 })
